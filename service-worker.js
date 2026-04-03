6d0eb120c670b1c00e3089e73cf7153de9d8bb2e
Paschal-hub39 <azubuikepaschal6@gmail.com>
2026-04-03 16:35:43 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,17 +1,22 @@
-const CACHE_NAME = "moneyhub-v1";
+const cacheName = "paschala-hub-cache-v1";
+const assetsToCache = [
+  "/",
+  "/index.html",
+  "/admin.html",
+  "/style.css",
+  "/ei_1775215120589-removebg-preview_1~2.png",
+  "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css",
+  "https://cdnjs.cloudflare.com/ajax/libs/Sortable/1.15.0/Sortable.min.js"
+];
 
-self.addEventListener("install", e=>{
+self.addEventListener("install", e => {
   e.waitUntil(
-    caches.open(CACHE_NAME).then(cache=>{
-      return cache.addAll(["/","/index.html"]);
-    })
+    caches.open(cacheName).then(cache => cache.addAll(assetsToCache))
   );
 });
 
-self.addEventListener("fetch", e=>{
+self.addEventListener("fetch", e => {
   e.respondWith(
-    caches.match(e.request).then(res=>{
-      return res || fetch(e.request);
-    })
+    caches.match(e.request).then(resp => resp || fetch(e.request))
   );
 });
